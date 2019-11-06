3d4da858f71e938cddadfa3f0a88fd35586b168b
jacsongomez <57466098+jacsongomez@users.noreply.github.com>
2019-11-06 18:31:38 -0500

    Add files via upload

---
+++ info_productos.java
@@ -0,0 +1,8 @@
+package hackathon;
+
+public class info_productos {
+    
+    public String nombrep;
+    public int codigop;
+    
+}
---
+++ info_usuario.java
@@ -0,0 +1,8 @@
+package hackathon;
+
+public class info_usuario {
+
+    public String nombre;
+    public int codigo;
+    public int cedula;
+}
